57d8fd491f08db5b6d33c1bcbafb82dfd0104be9
Alexey <alex1976212@gmail.com>
2023-11-23 23:12:44 +0300

    just commit

---
+++ src/main/java/org/example/Parser.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Parser {
+    public static void main(String[] args) {
+        int d = 10;
+        System.out.println(+-+d);
+        System.out.println(d);
+
+        int a = -40;
+        System.out.println(~a);
+    }
+}
